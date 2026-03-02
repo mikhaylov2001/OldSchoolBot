3ecaa93dae945a1448e40a83c36695e5169e5c7b
dimon9995 <mikhailov1500@mail.ru>
2026-03-03 01:41:59 +0300

    updated pages

--- src/main/java/com/example/oldschoolclub/config/BotConfig.java
+++ src/main/java/com/example/oldschoolclub/config/BotConfig.java
@@ -2,6 +2,7 @@ package com.example.oldschoolclub.config;
 
 import com.example.oldschoolclub.bot.OldSchoolBot;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,16 +10,17 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Configuration
-@RequiredArgsConstructor
-public class BotConfig {
+    @Configuration
+    @RequiredArgsConstructor
+    public class BotConfig {
 
-    private final OldSchoolBot oldSchoolBot;
+        private final OldSchoolBot oldSchoolBot;
 
-    @EventListener({ContextRefreshedEvent.class})
-    public void init() throws TelegramApiException {
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(oldSchoolBot);
-        System.out.println("✅ Бот запущен!");
+        @Bean
+        public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(oldSchoolBot);
+            System.out.println("✅ Бот запущен!");
+            return telegramBotsApi;
+        }
     }
-}
