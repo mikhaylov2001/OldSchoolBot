1e2b9b4c5a491ccd3b5df00de8c93f7ebd929cd8
dimon9995 <mikhailov1500@mail.ru>
2026-03-04 17:31:39 +0300

    updated

---
+++ src/main/java/com/example/oldschoolclub/bot/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.oldschoolclub.bot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
